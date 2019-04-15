5877b354e4c96fc2e1bb16798024567682ecf8cc
uttomain <uttomain@gmail.com>
2019-04-15 23:08:29 +0200

    Initial Commit

---
+++ SoApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SoApp.API.Models;
+
+namespace SoApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
