20ebb10636336cdc8c9e168e55e01bf3c823e66b
uttomain <uttomain@gmail.com>
2019-04-18 10:25:19 +0200

    latest break 4.2

--- SoApp-SPA/src/app/app.module.ts
+++ SoApp-SPA/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
-   bootstrap: [
-      AppComponent
+   providers: [
+      AuthService
    ]
 })
 export class AppModule { }
