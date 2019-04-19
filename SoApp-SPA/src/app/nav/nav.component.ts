fef7cf69441b5c6ca72e17fe1c70a90dca14ed34
uttomain <uttomain@gmail.com>
2019-04-19 16:11:40 +0200

    End of section 4

--- SoApp-SPA/src/app/nav/nav.component.ts
+++ SoApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) {  }
 
   ngOnInit() {
   }
 
   login() {
+      this.authService.login(this.model).subscribe(next => {
+      console.log('You Are Loggin in');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  // token; is like a if statement
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
   }
 
 }
