2624d183fad85fcf07df803e3c9b072e8a16b0a8
uttomain <uttomain@gmail.com>
2019-04-20 16:54:14 +0200

    ErrorHelpers Done

--- SoApp-SPA/src/app/nav/nav.component.ts
+++ SoApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
       this.authService.login(this.model).subscribe(next => {
       console.log('You Are Loggin in');
     }, error => {
-      console.log('Failed to login');
+      console.log(error);
     });
   }
 
--- SoApp.API/Controllers/AuthController.cs
+++ SoApp.API/Controllers/AuthController.cs
@@ -47,6 +47,8 @@ namespace SoApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
