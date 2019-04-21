a676587a99c71a53b39e3e3ced5cc0b4ec5a1bff
uttomain <uttomain@gmail.com>
2019-04-21 14:04:08 +0200

    third party Lib/stylling Added

--- SoApp-SPA/src/app/_services/auth.service.ts
+++ SoApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) { }
   login(model: any) {
@@ -16,12 +19,19 @@ constructor(private http: HttpClient) { }
           const user = response;
            if (user) {
              localStorage.setItem('token', user.token);
+             this.decodedToken = this.jwtHelper.decodeToken(user.token);
+             console.log(this.decodedToken);
            }
         })
       );
   }
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
 
