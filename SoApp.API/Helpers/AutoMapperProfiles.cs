90b37aa77fdfb305457bee9b5dcf1f4da5136a62
uttomain <uttomain@gmail.com>
2019-05-22 18:58:12 +0200

    Member Edit Component Added

--- SoApp.API/Helpers/AutoMapperProfiles.cs
+++ SoApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace SoApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>(); 
+            CreateMap<UserForUpdateDto, User>(); 
         }
     }
 }
